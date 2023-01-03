7420947b33c4f212d61bea7b05aafafcb92db56b
mohammedshanan <mohammedshanan12@gmail.com>
2023-01-03 20:48:02 +0200

    solved tebahpla

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(26):
     print(
-        "{}".format(chr(ord('z') - i)if i % 2 == 0 else chr(ord('z') - i - 32))\
-            , end="")
+        "{}".format(chr(ord('z') - i)
+        if i % 2 == 0 else chr(ord('z') - i - 32)), end="")

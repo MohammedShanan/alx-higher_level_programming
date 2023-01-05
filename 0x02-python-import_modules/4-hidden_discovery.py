2ea2dc6563a24406aa4fa9996840c60e21252e3f
mohammedshanan <mohammedshanan12@gmail.com>
2023-01-05 10:53:19 +0200

    solved infinite add

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def main():
+    names = filter(lambda x: not x.stratswith('__'), dir(hid4))
+    print("\n".join(names))
+
+
+if __name__ == "__main__":
+    import hidden_4 as hid4
+    main()

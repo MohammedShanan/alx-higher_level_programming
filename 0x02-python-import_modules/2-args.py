1785cec0442acecec55adf21222e989de57a9cf9
mohammedshanan <mohammedshanan12@gmail.com>
2023-01-05 10:20:18 +0200

    solved argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from sys import argv
+
+
+def main():
+    user_input = argv[1:]
+    argc = len(user_input)
+    if argc == 0:
+        print("0 arguments.")
+    else:
+        print(f"{argc} arguments:")
+    for i in range(argc):
+        print(f"{i + 1}: {user_input[i]}")
+
+
+if __name__ == "__main__":
+    main()

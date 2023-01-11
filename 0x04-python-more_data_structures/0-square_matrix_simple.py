7e9a55271ad85b8c5b222a35b9045b6f5c3ad90e
mohammedshanan <mohammedshanan12@gmail.com>
2023-01-11 14:33:58 +0200

    initail commit

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,3 @@
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix.copy()
-    return list(map(lambda x: map(lambda y: y ** 2, x), new_matrix)
+    return list(map(lambda x: list(map(lambda y: y ** 2, x)), matrix.copy()))

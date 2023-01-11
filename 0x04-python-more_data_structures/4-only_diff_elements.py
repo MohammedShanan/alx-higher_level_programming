e91f83d0bd9b5f0f71bbf71db8f75e45ce0cf539
mohammedshanan <mohammedshanan12@gmail.com>
2023-01-11 15:09:51 +0200

    solved diff

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    list1 = list(filter(lambda x: x not in set_1, set_2))
+    list2 = list(filter(lambda x: x not in set_2, set_1))
+    return list1 + list2

1a89aa11cd50610e086fa63b22cee61467d77777
mohammedshanan <mohammedshanan12@gmail.com>
2023-01-11 16:03:19 +0200

    solved value * 2:

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,4 +2,4 @@
 def multiply_by_2(a_dictionary):
     return {key: value * 2 for key, value in a_dictionary.items()}
 
-# return dict(zip(a_dictionary.keys(),map(lambda x: a_dictionary[x] * 2, a_dictionary)))
+# dict(zip(a_dictionary.keys(),map(lambda x: a_dictionary[x] * 2, a_dictionary)))

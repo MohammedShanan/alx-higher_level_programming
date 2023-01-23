92d5341f2e6f8b149d7cd886d8ecabed86bc3690
mohammedshanan <mohammedshanan12@gmail.com>
2023-01-23 11:07:37 +0200

    solved safe func

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    counter = i = 0
-    while i < x:
+    counter = 0
+    for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
             counter += 1

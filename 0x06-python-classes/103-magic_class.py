a9bdc9653a8f1d42bb319359876784f86fc71f6d
mohammedshanan <mohammedshanan12@gmail.com>
2023-01-24 17:40:17 +0200

    solved magic class

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -5,7 +5,7 @@ import math
 
 class MagicClass:
     """Initialize and define methods area and circumference"""
-    def __init__(self, radius):
+    def __init__(self, radius=0):
         """Initialize MagicClass"""
         self.__radius = 0
         if type(radius) is not int or type(radius) is not float:

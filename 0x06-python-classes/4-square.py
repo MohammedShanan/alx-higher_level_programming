cada0770e2fa6a92510a1663dc6ce4ab01be0da3
mohammedshanan <mohammedshanan12@gmail.com>
2023-01-24 15:59:09 +0200

    soolved setter and getter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+    module 4-square
+    defines class square
+    """
+
+
+class Square:
+    """
+    class square definition
+    Args:
+        size: size of the square
+    """
+
+    def __init__(self, size=0):
+        """
+        initialize square
+        Attributes:
+            size (int): size of the square
+        """
+
+    @property
+    def size(self):
+        """Getter
+
+        Returns:size        
+            """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter
+
+        Args:
+            value (int): size value
+        Raises:
+            TypeError: size not and int 
+            ValueError: size < 0
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        calculate the area of the square
+        Returns:
+            area
+        """
+        return self.__size ** 2

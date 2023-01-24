bf4ff3eb1c021793c862cb0f65d34dd99610c451
mohammedshanan <mohammedshanan12@gmail.com>
2023-01-24 15:47:50 +0200

    solved square area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+    module 2-square
+    defines class square
+    """
+
+
+class Square:
+    """
+    class square definition
+    Args:
+        size: size of the square
+    """
+
+    def __init__(self, size=0):
+        """
+        initialize square
+        Attributes:
+            size (int): size of the square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        calculate the area of the square
+        Returns:
+            area
+        """
+        return self.__size ** 2

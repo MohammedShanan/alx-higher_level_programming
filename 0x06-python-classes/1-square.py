a6778b853e13bea1d13592de9e134f9a78a51441
mohammedshanan <mohammedshanan12@gmail.com>
2023-01-24 15:10:23 +0200

    initial commit privte size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    module 1-square
+    defines class square
+    """
+
+
+class Square:
+    """
+    class square definition
+    Args:
+        size: size of the square
+    """
+
+    def __init__(self, size):
+        """
+        initialize square
+        Attributes:
+            size: size of the square
+        """
+        self.__size = size

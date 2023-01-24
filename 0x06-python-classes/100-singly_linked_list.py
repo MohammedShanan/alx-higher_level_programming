186585601600241cc28fbdb4d1b8c1378b0d1597
mohammedshanan <mohammedshanan12@gmail.com>
2023-01-24 20:19:42 +0200

    solved linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -130,18 +130,3 @@ class SinglyLinkedList:
             if tmp is not None:
                 string += "\n"
         return string
-
-
-sll = SinglyLinkedList()
-sll.sorted_insert(2)
-sll.sorted_insert(5)
-sll.sorted_insert(3)
-sll.sorted_insert(10)
-sll.sorted_insert(1)
-sll.sorted_insert(-4)
-sll.sorted_insert(-3)
-sll.sorted_insert(4)
-sll.sorted_insert(5)
-sll.sorted_insert(12)
-sll.sorted_insert(3)
-print(sll)

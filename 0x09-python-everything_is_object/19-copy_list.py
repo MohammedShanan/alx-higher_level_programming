c6149fae0aca8cf40214e38fbf1018ff9f547bad
mohammedshanan <mohammedshanan12@gmail.com>
2023-01-31 11:58:31 +0200

    intial commit

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+"""
+Module 0-add_integer
+Contains one method that returns an int sum
+Accepts two values, whether int or float, and casts them to int before adding
+"""
 def copy_list(l):
     return l.copy()

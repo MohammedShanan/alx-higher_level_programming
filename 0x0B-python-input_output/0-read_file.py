3587340292647859499e181b728bc4d8a1e905b7
mohammedshanan <mohammedshanan12@gmail.com>
2023-02-07 09:46:26 +0200

    solved f.write

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,5 +8,5 @@ Contains function that reads and prints contents from file
 
 def read_file(filename=""):
     """Read and print text from file"""
-    with open(filename, mode="r", encoding="utf8") as f:
+    with open(filename, mode="r", encoding="utf-8") as f:
         print(f.read(), end="")

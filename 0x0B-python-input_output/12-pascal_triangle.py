cf1d259e3b3e6dadefc013607c47621396bd5dd9
mohammedshanan <mohammedshanan12@gmail.com>
2023-02-08 00:43:35 +0200

    solved pascal triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -24,6 +24,6 @@ def pascal_triangle(n):
 
     triangle = [[1]]
     for _ in range(n - 1):
-        triangle += [a+b for a, b
-                     in zip([0] + triangle[-1], triangle[-1] + [0])]
+        triangle.append([a+b for a, b
+                         in zip([0] + triangle[-1], triangle[-1] + [0])])
     return triangle

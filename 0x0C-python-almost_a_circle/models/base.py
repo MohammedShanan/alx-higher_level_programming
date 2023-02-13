eae046860f8c6604e13cd7656dcc1fd918dc0318
mohammedshanan <mohammedshanan12@gmail.com>
2023-02-13 19:37:24 +0200

    added test for square

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,7 +16,7 @@ import json
 import csv
 
 
-class Base():
+class Base:
     """
     defines class Base
     Class Attributes:
@@ -30,6 +30,7 @@ class Base():
         load_from_file(cls)                 load_from_file_csv(cls)
         create(cls, **dictionary)
     """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
@@ -93,7 +94,7 @@ class Base():
     def save_to_file_csv(cls, list_objs):
         """Save all instances attributes into csv file"""
         filename = cls.__name__ + ".csv"
-        with open(filename, 'w', newline='') as f:
+        with open(filename, "w", newline="") as f:
             writer = csv.writer(f)
             for o in list_objs:
                 if cls.__name__ == "Rectangle":
@@ -106,20 +107,24 @@ class Base():
         """Returns list of instances"""
         objs = []
         filename = cls.__name__ + ".csv"
-        with open(filename, 'r', newline='') as f:
+        with open(filename, "r", newline="") as f:
             reader = csv.reader(f)
             for row in reader:
                 if cls.__name__ == "Rectangle":
-                    dic = {"id": int(row[0]),
-                           "width": int(row[1]),
-                           "height": int(row[2]),
-                           "x": int(row[3]),
-                           "y": int(row[4])}
+                    dic = {
+                        "id": int(row[0]),
+                        "width": int(row[1]),
+                        "height": int(row[2]),
+                        "x": int(row[3]),
+                        "y": int(row[4]),
+                    }
                 if cls.__name__ == "Square":
-                    dic = {"id": int(row[0]),
-                           "size": int(row[1]),
-                           "x": int(row[2]),
-                           "y": int(row[3])}
+                    dic = {
+                        "id": int(row[0]),
+                        "size": int(row[1]),
+                        "x": int(row[2]),
+                        "y": int(row[3]),
+                    }
                 o = cls.create(**dic)
                 objs.append(o)
         return objs

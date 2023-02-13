43835b6b8c943c2488137d6f2f1417a681f3a5d1
mohammedshanan <mohammedshanan12@gmail.com>
2023-02-13 21:22:16 +0200

    added test for square

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,6 +14,9 @@ Saves to CSV and loads from CSV file
 
 import json
 import csv
+import turtle
+import time
+from itertools import repeat
 
 
 class Base():
@@ -130,3 +133,35 @@ class Base():
                 o = cls.create(**dic)
                 objs.append(o)
         return objs
+
+    def draw(list_rectangles, list_squares):
+        window = turtle.Screen()
+        window.setup(width=1.0, height=1.0)
+        turtle.speed(3)
+        for i, obj in enumerate(list_rectangles):
+            turtle.penup()
+            turtle.goto(-400, (obj.height * 50) // 2 - 10)
+            turtle.write(
+                f"Rectangle Number {i + 1} ---->",
+                font=("Arial", 20, "normal"))
+            turtle.goto(0, 0)
+            turtle.pendown()
+            parameters = [obj.width, obj.height] * 2
+            for size in parameters:
+                turtle.forward(size * 50)
+                turtle.left(90)
+            time.sleep(0.5)
+            turtle.clear()
+
+        for i, obj in enumerate(list_squares):
+            turtle.penup()
+            turtle.goto(-400, (obj.size * 50) // 2 - 10)
+            turtle.write(f"Square Number {i + 1} ---->",
+                         font=("Arial", 20, "normal"))
+            turtle.goto(0, 0)
+            turtle.pendown()
+            for _ in range(4):
+                turtle.forward(obj.size * 50)
+                turtle.left(90)
+            time.sleep(0.5)
+            turtle.clear()
--- 0x0C-python-almost_a_circle/t.py
+++ 0x0C-python-almost_a_circle/t.py
@@ -3,7 +3,15 @@
 """ 18-main """
 from models.rectangle import Rectangle
 from models.square import Square
-
+r1 = Rectangle(5, 3)
+r2 = Rectangle(5, 2)
+r3 = Rectangle(3, 1)
+r4 = Rectangle(6, 4)
+s1 = Square(5)
+s2 = Square(4)
+s3 = Square(3)
+s4 = Square(10)
+Rectangle.draw([r1, r2], [s1, s2, s3, s4])
 s1 = Square(5)
 s2 = Square(7, 9, 1)
 list_squares_input = [s1, s2]
@@ -18,4 +26,4 @@ for square in list_squares_input:
 print("---")
 
 for square in list_squares_output:
-        print("[{}] {}".format(id(square), square))
+    print("[{}] {}".format(id(square), square))

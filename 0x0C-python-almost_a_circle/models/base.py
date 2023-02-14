a16fd18039005e09b3321a513e2167512e17b70e
mohammedshanan <mohammedshanan12@gmail.com>
2023-02-14 11:51:50 +0200

    test

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,9 +14,6 @@ Saves to CSV and loads from CSV file
 
 import json
 import csv
-import turtle
-import time
-from itertools import repeat
 
 
 class Base():
@@ -27,13 +24,10 @@ class Base():
     Methods:
         __init__(self, id=None)
     Static Methods:
-        to_json_string(list_dictionaries)
-        from_json_string(json_string)
+        to_json_string(list_dictionaries)   from_json_string(json_string)
     Class Methods:
-        save_to_file(cls, list_objs)
-        save_to_file_csv(cls, list_objs)
-        load_from_file(cls)
-        load_from_file_csv(cls)
+        save_to_file(cls, list_objs)        save_to_file_csv(cls, list_objs)
+        load_from_file(cls)                 load_from_file_csv(cls)
         create(cls, **dictionary)
     """
     __nb_objects = 0
@@ -84,22 +78,21 @@ class Base():
     @classmethod
     def load_from_file(cls):
         """Returns list of instances"""
-        filename = cls.__name__ + ".json"
-        instances_list = []
+        filenamee = cls.__name__ + ".json"
+        l = []
         try:
-            with open(filename, "r") as f:
+            with open(filenamee, "r") as f:
                 instances = cls.from_json_string(f.read())
-            for dic in instances:
-                instances_list.append(cls.create(**dic))
-        except FileNotFoundError:
-            return []
-        return instances_list
+            for i, dic in enumerate(instances):
+                l.append(cls.create(**instances[i]))
+        except:
+            pass
+        return l
 
     @classmethod
     def save_to_file_csv(cls, list_objs):
-        """Save all instances attributes into csv file"""
         filename = cls.__name__ + ".csv"
-        with open(filename, "w", newline="") as f:
+        with open(filename, 'w', newline='') as f:
             writer = csv.writer(f)
             for o in list_objs:
                 if cls.__name__ == "Rectangle":
@@ -109,29 +102,22 @@ class Base():
 
     @classmethod
     def load_from_file_csv(cls):
-        """Returns list of instances"""
         objs = []
         filename = cls.__name__ + ".csv"
-        with open(filename, "r", newline="") as f:
+        with open(filename, 'r', newline='') as f:
             reader = csv.reader(f)
             for row in reader:
                 if cls.__name__ == "Rectangle":
-                    dic = {
-                        "id": int(row[0]),
-                        "width": int(row[1]),
-                        "height": int(row[2]),
-                        "x": int(row[3]),
-                        "y": int(row[4]),
-                    }
+                    dic = {"id": int(row[0]),
+                           "width": int(row[1]),
+                           "height": int(row[2]),
+                           "x": int(row[3]),
+                           "y": int(row[4])}
                 if cls.__name__ == "Square":
-                    dic = {
-                        "id": int(row[0]),
-                        "size": int(row[1]),
-                        "x": int(row[2]),
-                        "y": int(row[3]),
-                    }
+                    dic = {"id": int(row[0]),
+                           "size": int(row[1]),
+                           "x": int(row[2]),
+                           "y": int(row[3])}
                 o = cls.create(**dic)
                 objs.append(o)
         return objs
-
-

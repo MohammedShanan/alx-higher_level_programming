35d53008ab1ba00f1e7f5953803f1c086348777f
mohammedshanan <mohammedshanan12@gmail.com>
2023-03-14 10:58:48 +0200

    solved 13 14 15 16

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,9 +1,3 @@
 -- Update data in table; change Bob's score to 10
---
--- score   name
--- 10  Bob
--- 10  John
--- 8   George
--- 3   Alex
 
 UPDATE second_table SET `score` = 10 WHERE `name` = 'Bob';

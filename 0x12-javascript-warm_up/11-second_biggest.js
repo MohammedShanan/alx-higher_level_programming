2e33f723a21bb7f8ed8a0ca0eec5dbe74c727e40
mohammedshanan <mohammedshanan12@gmail.com>
2023-04-11 07:50:49 +0200

    solved square

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const numsArray = process.argv.slice(2);
+function secondMax (array) {
+  let max = -Infinity;
+  let secMax = -Infinity;
+  if (array.length < 2) {
+    return (0);
+  }
+  for (num of array) {
+    if (num > max) {
+        secMax = max;
+        max = num;
+    } else if (num < max && num > secMax) {
+      secMax = num;
+    }
+  }
+  return (secMax);
+}
+console.log(secondMax(numsArray));

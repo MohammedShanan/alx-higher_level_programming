05ebb92dcac363dde7f29f352ac6a2a9ae4d9f4f
mohammedshanan <mohammedshanan12@gmail.com>
2023-04-11 11:52:45 +0200

    fixed export

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -4,6 +4,6 @@ const fs = require('fs');
 const fileA = process.argv[2];
 const fileB = process.argv[3];
 const fileC = process.argv[4];
-const textA = readFileSync(fileA, 'utf8');
-const textB = readFileSync(fileB, 'utf8');
+const textA = fs.readFileSync(fileA, 'utf8');
+const textB = fs.readFileSync(fileB, 'utf8');
 writeFileSync(fileC, textA + textB);

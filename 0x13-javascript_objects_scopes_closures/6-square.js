7fe4adc7a7ebe434fe9992605e4dfabaa6287bf2
mohammedshanan <mohammedshanan12@gmail.com>
2023-04-12 09:16:50 +0200

    fixed imporrt

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,17 +1,13 @@
 #!/usr/bin/node
-import Rectangle from './4-rectangle';
+const BaseSquare = require('./5-square');
 
-module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
-  charPrint (c) {
+module.exports = class Square extends BaseSquare {
+  charPrint(c) {
     if (c === undefined) {
       this.print();
     } else {
-      for (let col = 0; col < this.height; col++) {
-        console.log(c.repeat(this.width));
+      for (let col = 0; col < this.width; col += 1) {
+        console.log(c.repeat(this.height));
       }
     }
   }

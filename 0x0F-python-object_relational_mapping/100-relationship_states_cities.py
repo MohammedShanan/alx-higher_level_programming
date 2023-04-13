2dbacf40e5da58cbaca1c77ced64b1f4807446b8
mohammedshanan <mohammedshanan12@gmail.com>
2023-04-13 16:56:49 +0200

    solved one to many relationship

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+return all cities from database via python
+parameters given to script: username, password, database
+"""
+
+
+
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    
+    session = sessionmaker(bind=engine)
+    session = session()
+
+    new_state = State(name="California")
+    new_city = City(name="San Francisco", state=new_state)
+    
+    session.add_all([new_state, new_city])
+    session.commit()
+    session.close()

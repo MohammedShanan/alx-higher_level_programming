2311b00546b5840670d06f63900434806b1bb095
mohammedshanan <mohammedshanan12@gmail.com>
2023-04-13 10:02:09 +0200

    solved Cities by states

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,25 @@
+#!/bin/bash/python3
+
+"""
+return info from both tables (tables 'cities' 'states)
+parameters given to script: username, password, database
+"""
+
+import MySQLdb
+from sys import argv
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cursor = db.cursor()
+    sql_cmd = """SELECT cities.id, cities.name, states.name
+                 FROM states JOIN cities
+                 WHERE cities.state_id = states.id
+                 ORDER BY cities.id ASC"""
+    cursor.execute(sql_cmd)
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    db.close()

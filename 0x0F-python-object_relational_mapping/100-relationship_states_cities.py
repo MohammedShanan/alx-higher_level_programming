ffe35cf72fc81b94b7898a389a9c776dfefb47ed
mohammedshanan <mohammedshanan12@gmail.com>
2023-04-14 12:19:41 +0200

    fixed for loop

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -5,7 +5,6 @@ parameters given to script: username, password, database
 """
 
 
-
 import sys
 from relationship_state import Base, State
 from relationship_city import City
@@ -16,13 +15,13 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    
+
     session = sessionmaker(bind=engine)
     session = session()
 
     new_state = State(name="California")
     new_city = City(name="San Francisco", state=new_state)
-    
+
     session.add_all([new_state, new_city])
     session.commit()
     session.close()
--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -5,7 +5,6 @@ parameters given to script: username, password, database
 """
 
 
-
 import sys
 from relationship_state import Base, State
 from relationship_city import City
@@ -15,7 +14,7 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    
+
     session = sessionmaker(bind=engine)
     session = session()
 
@@ -24,6 +23,5 @@ if __name__ == "__main__":
         print("{:d}: {:s}".format(state.id, state.name))
         for city in state.cities:
             print("    {:d}: {:s}".format(city.id, city.name))
-            
-    
+
     session.close()

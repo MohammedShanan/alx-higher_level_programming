cb4314217f49c207db0cfce8ea7084e9350312ae
mohammedshanan <mohammedshana12@gmail.com>
2024-07-30 22:44:46 +0300

    solved Who was playing in this movie?

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const request = require("request");
+const request = require('request');
 const movieId = process.argv[2];
-const starWarsApi = "https://swapi-api.alx-tools.com/api/films/";
+const starWarsApi = 'https://swapi-api.alx-tools.com/api/films/';
 request.get(starWarsApi + movieId, function (err, response, body) {
   if (err) {
     console.log(err);

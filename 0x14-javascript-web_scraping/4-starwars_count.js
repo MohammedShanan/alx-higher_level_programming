7e9bb627a196a0bec146ece5fecc890955424d45
mohammedshanan <mohammedshana12@gmail.com>
2024-07-30 21:25:46 +0300

    solved Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -8,12 +8,12 @@ request.get(starWarsApi, function (err, response, body) {
     const movies = JSON.parse(body).results;
     let count = 0;
     for (let movie of movies) {
-      console.log(movie);
-      for (let Character of movie["Characters"]) {
-        console.log(Character);
-        if (Character.endsWith("/18/")) {
-          count++;
-        }
+      console.log(movie.Characters);
+    //   for (let Character of movie["Characters"]) {
+    //     console.log(Character);
+    //     if (Character.endsWith("/18/")) {
+    //       count++;
+    //     }
       }
     }
     console.log(count);

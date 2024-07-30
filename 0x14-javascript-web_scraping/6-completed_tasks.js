a35d43523b397b49130a4291f9720a612e8b76b4
mohammedshanan <mohammedshana12@gmail.com>
2024-07-30 21:49:49 +0300

    solved completed tasks

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+request.get(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const jsonResponse = JSON.parse(body);
+    const tasks = {};
+    for (const task of jsonResponse) {
+      if (task.completed === true) {
+        tasks[task.userId] = (tasks[task.userId] || 0) + 1;
+      }
+    }
+    console.log(tasks);
+  }
+});

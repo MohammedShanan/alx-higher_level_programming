e10a1210b70cc0d77c73c2acfdf717db1ad3187e
mohammedshanan <mohammedshana12@gmail.com>
2024-07-30 19:38:02 +0300

    initial commit

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
+
 const fs = require("fs");
-const filename = process.argv[1];
-fs.readFile(filename, "utf8", (err, data) => {
+const filename = process.argv[2];
+
+fs.readFile(filename, "utf8", function (err, data) {
   if (err) {
     console.log(err);
+  } else {
+    console.log(data);
   }
-  console.log(data);
 });

aa03ca93b0a9ec0c08b450cff95136d9a7c2667a
mohammedshanan <mohammedshana12@gmail.com>
2024-07-30 20:34:56 +0300

    solved starwars 1

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
-const request = require('request');
+const request = require("request");
 const movieId = process.argv[2];
-const starWarsApi = 'https://swapi-api.alx-tools.com/api/films/';
-request.get(starWarsApi + movieId, function (response, body) {
-  if (response.statusCode === 200) {
+const starWarsApi = "https://swapi-api.alx-tools.com/api/films/";
+request.get(starWarsApi + movieId, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
     const jsonResponse = JSON.parse(body);
     console.log(jsonResponse.title);
   }
